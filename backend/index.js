0a210ade0a84a25bbb3170d745ea20f42ba5e2ab
naXeer rehman <naxeerrehman.com>
2025-08-31 04:32:31 -0700

    New update

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import corsMiddleware from "./Middlewares/cors.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 5000;
 
